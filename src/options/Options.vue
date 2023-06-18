cd57b85e192e8113466dcca09067a8098b646b3d
Yuma Shiraishi <pharmacy.sy@gmail.com>
2023-06-18 23:04:06 +0900

    feature: API 設定の有効性確認 UI

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
+import { getMyself } from '~/api/backlog'
 import { storage } from '~/logic/storage'
+
+const status: Ref<'valid' | 'invalid' | 'unchecked'> = ref('unchecked')
+
+onMounted(async () => {
+  await validateConfigs()
+})
+
+async function validateConfigs() {
+  try {
+    // 有効性確認のために適当なエンドポイントを叩く
+    await getMyself()
+    status.value = 'valid'
+  }
+  catch {
+    status.value = 'invalid'
+  }
+}
 </script>
 
 <template>
@@ -8,7 +26,7 @@ import { storage } from '~/logic/storage'
       <h1 class="text-xl">
         Settings
       </h1>
-      <div class="flex flex-col space-y-5">
+      <div class="flex flex-col items-center space-y-5">
         <label class="flex flex-col items-start">
           <div>Backlog Host</div>
           <input
@@ -24,6 +42,32 @@ import { storage } from '~/logic/storage'
             class="border border-gray-400 rounded px-2 py-1"
           >
         </label>
+        <button
+          class="
+          bg-blue text-white p-2 rounded
+            transform active:scale-75 transition-transform
+          "
+          @click="validateConfigs"
+        >
+          <div class="flex flex-row space-x-1 items-center justify-center">
+            <pixelarticons-check />
+            <div>有効性のチェック</div>
+          </div>
+        </button>
+        <div
+          v-if="status === 'invalid'"
+          class="text-red text-xs flex flex-row space-x-1 items-center justify-center"
+        >
+          <pixelarticons-close />
+          <div>Backlog Host または Backlog API Key が正しくありません。</div>
+        </div>
+        <div
+          v-else-if="status === 'valid'"
+          class="text-green text-xs flex flex-row space-x-1 items-center justify-center"
+        >
+          <pixelarticons-check />
+          <div>Backlog Host と Backlog API Key が正しく設定されています。</div>
+        </div>
       </div>
     </div>
   </main>
