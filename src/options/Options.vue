c6553eba9746ada95f1d15883419cca6606b56b6
Yuma Shiraishi <pharmacy.sy@gmail.com>
2023-06-19 09:42:17 +0900

    style: setting page のレイアウト変更

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -26,48 +26,53 @@ async function validateConfigs() {
       <h1 class="text-xl">
         Settings
       </h1>
-      <div class="flex flex-col items-center space-y-5">
-        <label class="flex flex-col items-start">
-          <div>Backlog Host</div>
+      <div class="flex flex-col">
+        <label class="flex flex-row justify-start items-center space-x-3">
+          <div class="w-[10rem] text-right p-2 bg-gray-200">Backlog Host</div>
           <input
             v-model="storage.backlogHost"
             class="border border-gray-400 rounded px-2 py-1"
             placeholder="xxx.backlog.jp"
           >
         </label>
-        <label class="flex flex-col items-start">
-          <div>Backlog API Key</div>
+        <label class="flex flex-row justify-start items-center space-x-3">
+          <div class="w-[10rem] text-right p-2 bg-gray-200">Backlog API Key</div>
           <input
             v-model="storage.backlogApiKey"
             class="border border-gray-400 rounded px-2 py-1"
           >
         </label>
-        <button
-          class="
-          bg-blue text-white p-2 rounded
-            transform active:scale-75 transition-transform
-          "
-          @click="validateConfigs"
-        >
-          <div class="flex flex-row space-x-1 items-center justify-center">
-            <pixelarticons-check />
-            <div>有効性のチェック</div>
+        <label class="flex flex-row justify-start items-center space-x-3">
+          <div class="w-[10rem] text-right p-2 bg-gray-200 h-14">&nbsp;</div>
+          <div class="flex flex-col items-start space-y-1">
+            <button
+              class="
+              bg-blue text-white px-2 py-1 rounded
+                transform active:scale-75 transition-transform
+              "
+              @click="validateConfigs"
+            >
+              <div class="flex flex-row space-x-1 items-center justify-center">
+                <pixelarticons-check />
+                <div>有効性のチェック</div>
+              </div>
+            </button>
+            <div
+              v-if="status === 'invalid'"
+              class="text-red text-xs flex flex-row space-x-1 items-center justify-center"
+            >
+              <pixelarticons-close />
+              <div>Backlog Host または Backlog API Key が正しくありません。</div>
+            </div>
+            <div
+              v-else-if="status === 'valid'"
+              class="text-green text-xs flex flex-row space-x-1 items-center justify-center"
+            >
+              <pixelarticons-check />
+              <div>Backlog Host と Backlog API Key が正しく設定されています。</div>
+            </div>
           </div>
-        </button>
-        <div
-          v-if="status === 'invalid'"
-          class="text-red text-xs flex flex-row space-x-1 items-center justify-center"
-        >
-          <pixelarticons-close />
-          <div>Backlog Host または Backlog API Key が正しくありません。</div>
-        </div>
-        <div
-          v-else-if="status === 'valid'"
-          class="text-green text-xs flex flex-row space-x-1 items-center justify-center"
-        >
-          <pixelarticons-check />
-          <div>Backlog Host と Backlog API Key が正しく設定されています。</div>
-        </div>
+        </label>
       </div>
     </div>
   </main>
