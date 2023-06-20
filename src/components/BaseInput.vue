1dbe6f2ae2df334c4e79023fbb68be2d5b6a8612
Yuma Shiraishi <pharmacy.sy@gmail.com>
2023-06-20 23:41:22 +0900

    add: 共通部分を切り出した

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,6 @@
+<template>
+  <input
+    v-bind="$attrs"
+    class="border border-gray-400 rounded px-2 py-1"
+  >
+</template>
---
+++ src/components/LabeledItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <label class="flex flex-row justify-start items-center space-x-3">
+    <div class="w-[10rem] text-right p-2 bg-gray-200 self-stretch">
+      <slot name="label">
+        &nbsp;
+      </slot>
+    </div>
+    <slot name="content" />
+  </label>
+</template>
