3d86fe1aa573069087086d067af1728abd284985
Yuma Shiraishi <pharmacy.sy@gmail.com>
2023-06-20 23:42:08 +0900

    add: strage options

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -3,6 +3,9 @@ import { useStorageLocal } from '~/composables/useStorageLocal'
 export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo')
 
 export const storage = useStorageLocal('options', {
-  backlogHost: 'yoriso.backlog.com',
-  backlogApiKey: 'HZuu17lY0baV1qSwAhoGx2K71DP1H0cvhLPyNzHRXYvfEFwmxRieDEQCL40fXzui',
+  backlogHost: '',
+  backlogApiKey: '',
+  backlogIdPrefix: '',
+  enableInputPRTitle: true,
+  enableInputPRDescription: true,
 })
