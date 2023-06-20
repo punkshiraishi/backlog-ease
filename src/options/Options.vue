d1881f70f0d58d8c4d9f86a2dcedcf4974f87ad0
Yuma Shiraishi <pharmacy.sy@gmail.com>
2023-06-20 23:43:01 +0900

    feature: 設定画面に切り出したコンポーネントを適用; プレフィクス項目を追加

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -27,52 +27,66 @@ async function validateConfigs() {
         Settings
       </h1>
       <div class="flex flex-col">
-        <label class="flex flex-row justify-start items-center space-x-3">
-          <div class="w-[10rem] text-right p-2 bg-gray-200">Backlog Host</div>
-          <input
-            v-model="storage.backlogHost"
-            class="border border-gray-400 rounded px-2 py-1"
-            placeholder="xxx.backlog.jp"
-          >
-        </label>
-        <label class="flex flex-row justify-start items-center space-x-3">
-          <div class="w-[10rem] text-right p-2 bg-gray-200">Backlog API Key</div>
-          <input
-            v-model="storage.backlogApiKey"
-            class="border border-gray-400 rounded px-2 py-1"
-          >
-        </label>
-        <label class="flex flex-row justify-start items-center space-x-3">
-          <div class="w-[10rem] text-right p-2 bg-gray-200 h-14">&nbsp;</div>
-          <div class="flex flex-col items-start space-y-1">
-            <button
-              class="
-              bg-blue text-white px-2 py-1 rounded
-                transform active:scale-75 transition-transform
-              "
-              @click="validateConfigs"
-            >
-              <div class="flex flex-row space-x-1 items-center justify-center">
-                <pixelarticons-check />
-                <div>有効性のチェック</div>
+        <LabeledItem>
+          <template #label>
+            Backlog Host
+          </template>
+          <template #content>
+            <BaseInput
+              v-model="storage.backlogHost"
+              placeholder="xxx.backlog.jp"
+            />
+          </template>
+        </LabeledItem>
+        <LabeledItem>
+          <template #label>
+            Backlog API Key
+          </template>
+          <template #content>
+            <BaseInput v-model="storage.backlogApiKey" />
+          </template>
+        </LabeledItem>
+        <LabeledItem>
+          <template #content>
+            <div class="flex flex-col items-start space-y-1">
+              <button
+                class="
+                bg-blue text-white px-2 py-1 rounded
+                  transform active:scale-75 transition-transform
+                "
+                @click="validateConfigs"
+              >
+                <div class="flex flex-row space-x-1 items-center justify-center">
+                  <pixelarticons-check />
+                  <div>有効性のチェック</div>
+                </div>
+              </button>
+              <div
+                v-if="status === 'invalid'"
+                class="text-red text-xs flex flex-row space-x-1 items-center justify-center"
+              >
+                <pixelarticons-close />
+                <div>Backlog Host または Backlog API Key が正しくありません。</div>
               </div>
-            </button>
-            <div
-              v-if="status === 'invalid'"
-              class="text-red text-xs flex flex-row space-x-1 items-center justify-center"
-            >
-              <pixelarticons-close />
-              <div>Backlog Host または Backlog API Key が正しくありません。</div>
+              <div
+                v-else-if="status === 'valid'"
+                class="text-green text-xs flex flex-row space-x-1 items-center justify-center"
+              >
+                <pixelarticons-check />
+                <div>Backlog Host と Backlog API Key が正しく設定されています。</div>
+              </div>
             </div>
-            <div
-              v-else-if="status === 'valid'"
-              class="text-green text-xs flex flex-row space-x-1 items-center justify-center"
-            >
-              <pixelarticons-check />
-              <div>Backlog Host と Backlog API Key が正しく設定されています。</div>
-            </div>
-          </div>
-        </label>
+          </template>
+        </LabeledItem>
+        <LabeledItem />
+        <LabeledItem>
+          <template #label>
+            Backlog ID Prefix
+          </template>
+          <template #content>
+            <BaseInput v-model="storage.backlogIdPrefix" />
+          </template>
+        </LabeledItem>
       </div>
     </div>
   </main>
