296312eaee7621950fd48d04c393e4ca1e315fbf
Yuma Shiraishi <pharmacy.sy@gmail.com>
2023-06-26 23:25:10 +0900

    fix: v-model を BaseInput で使えるようにした

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+import { useVModel } from '@vueuse/core'
+
+const props = defineProps({
+  modelValue: String,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const _modelValue = useVModel(props, 'modelValue', emit)
+</script>
+
 <template>
   <input
     v-bind="$attrs"
+    v-model="_modelValue"
     class="border border-gray-400 rounded px-2 py-1"
   >
 </template>
