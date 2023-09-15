e131dc3f892fdd6d1e04b515bcf8f444d5c58c73
punkshiraishi <pharmacy.sy@gmail.com>
2023-09-16 00:30:42 +0900

    install: unocss-preset-scrollbar

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'unocss/vite'
 import { presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
+import { presetScrollbar } from 'unocss-preset-scrollbar'
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
     presetIcons(),
+    presetScrollbar(),
   ],
   transformers: [
     transformerDirectives(),
