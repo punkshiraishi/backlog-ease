1f35b884ed71bc2611c16bcf5753a2b920a376d7
punkshiraishi <pharmacy.sy@gmail.com>
2023-09-15 21:33:33 +0900

    add: BaseToggle compact style

--- src/components/BaseToggle.vue
+++ src/components/BaseToggle.vue
@@ -6,6 +6,10 @@ const props = defineProps({
     type: Boolean,
     required: true,
   },
+  compact: {
+    type: Boolean,
+    default: false,
+  },
 })
 
 const emit = defineEmits(['update:modelValue'])
@@ -14,9 +18,23 @@ const _modelValue = useVModel(props, 'modelValue', emit)
 </script>
 
 <template>
-  <label class="relative inline-flex items-center cursor-pointer">
+  <label
+    :class="{ 'w-8 h-4': compact, 'w-11 h-6': !compact }"
+    class="relative inline-flex items-center cursor-pointer"
+  >
     <input v-model="_modelValue" type="checkbox" class="sr-only peer">
-    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary" />
+    <div
+      :class="compact
+        ? 'w-7 h-4 after:h-3 after:w-3 after:top-[2px] after:left-[2px]'
+        : 'w-11 h-6 after:h-6 after:w-6 after:top-[2px] after:left-[2px]'
+      "
+      class="
+        bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 dark:peer-focus:ring-blue-800 rounded-full
+        peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white
+        after:content-[''] after:absolute  after:bg-white after:border-gray-300 after:border after:rounded-full
+        after:transition-all dark:border-gray-600 peer-checked:bg-primary
+      "
+    />
   </label>
 </template>
 
