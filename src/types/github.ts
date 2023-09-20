50b965b8b88002ca6522eca46ae241546ba913fe
punkshiraishi <pharmacy.sy@gmail.com>
2023-09-20 21:02:13 +0900

    add: PR のラベル型を追加

--- src/types/github.ts
+++ src/types/github.ts
@@ -1,3 +1,4 @@
 import type githubPullRequestSample from './github-pull-request-sample.json'
 
 export type GithubPullRequest = typeof githubPullRequestSample
+export type GithubPullRequestLabel = GithubPullRequest['labels'][0]
