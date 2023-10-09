a25c5de84959c9d3b525bf23f054a83bbe04e635
punkshiraishi <pharmacy.sy@gmail.com>
2023-10-09 23:35:05 +0900

    setup: floating-vue

--- src/logic/common-setup.ts
+++ src/logic/common-setup.ts
@@ -1,4 +1,5 @@
 import type { App } from 'vue'
+import FloatingVue from 'floating-vue'
 
 export function setupApp(app: App) {
   // Inject a globally available `$app` object in template
@@ -9,7 +10,6 @@ export function setupApp(app: App) {
   // Provide access to `app` in script setup with `const app = inject('app')`
   app.provide('app', app.config.globalProperties.$app)
 
-  // Here you can install additional plugins for all contexts: popup, options page and content-script.
-  // example: app.use(i18n)
-  // example excluding content-script context: if (context !== 'content-script') app.use(i18n)
+  // floating-vue
+  app.use(FloatingVue)
 }
