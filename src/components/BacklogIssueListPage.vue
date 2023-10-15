244970245a93391fd5b6a3776bbff9e68108b4b3
punkshiraishi <pharmacy.sy@gmail.com>
2023-10-16 00:18:36 +0900

    add: BugButton

--- src/components/BacklogIssueListPage.vue
+++ src/components/BacklogIssueListPage.vue
@@ -146,8 +146,5 @@ async function _getGithubPullRequests(issue: BacklogIssue) {
     <div>
       {{ errorMessage }}
     </div>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeNXSKBPpXd8SVLbYSyavpY0dIbvYxRRIdR6p_TPFd59s-qiQ/viewform?usp=sf_link">
-      <ic:baseline-bug-report text-gray-500 text-2xs absolute right-0 />
-    </a>
   </main>
 </template>
---
+++ src/components/BugButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <a href="https://docs.google.com/forms/d/e/1FAIpQLSeNXSKBPpXd8SVLbYSyavpY0dIbvYxRRIdR6p_TPFd59s-qiQ/viewform?usp=sf_link">
+    <ic:baseline-bug-report text-gray-500 text-2xs />
+  </a>
+</template>
