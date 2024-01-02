8327bf2e78af6938f4f510f1cecb444e9ed02e41
punkshiraishi <punkshiraishi@gmail.com>
2024-01-03 00:45:35 +0900

    style: 設定項目ラベルの色を修正

--- src/components/LabeledItem.vue
+++ src/components/LabeledItem.vue
@@ -1,6 +1,6 @@
 <template>
   <label class="flex flex-col justify-start items-center w-full">
-    <div class="text-left p-2 w-full">
+    <div class="text-left py-2 w-full font-bold">
       <slot name="label">
         &nbsp;
       </slot>
