292c98cfe87b24536af1d592bae467e00881512c
Yuma Shiraishi <yuma-shiraishi@yoriso.com>
2025-06-06 17:42:24 +0900

    Fix: 課題一覧の背景色が途切れている問題を修正

--- src/dashboard/Dashboard.vue
+++ src/dashboard/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <main
-    w-screen bg-blueGray-100 p-10 h-screen
+    w-screen bg-blueGray-100 p-10 min-h-screen
     flex flex-col items-center
   >
     <div flex flex-col items-center relative w-700px>
